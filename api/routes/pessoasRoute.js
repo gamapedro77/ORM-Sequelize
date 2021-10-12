b69dad6182d17a01dc3432003c30f4ba3f44a593
PedrocaMarmota <pedrolucasdgm@gmail.com>
2021-10-12 16:20:00 -0300

    criação da rota de pegar pessoa por ID

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require("../controllers/PessoaController");
 const router = Router();
 
 router.get("/pessoas", PessoaController.listarTodas);
+router.get("/pessoas/:id", PessoaController.listarPorId);
 
 module.exports = router;
